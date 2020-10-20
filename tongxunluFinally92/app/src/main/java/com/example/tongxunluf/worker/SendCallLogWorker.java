f6b3f1e658c25bcae4a2ba3889ecac592bbf37eb
REN <ren39376659@hotmail.com>
2020-10-20 17:37:18 +0800

    添加worker方法，为循环执行做准备

---
+++ tongxunluFinally92/app/src/main/java/com/example/tongxunluf/worker/SendCallLogWorker.java
@@ -0,0 +1,25 @@
+package com.example.tongxunluf.worker;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class SendCallLogWorker extends Worker {
+
+    public SendCallLogWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    /**
+     * 将需要自行的方法在doWork中调用
+     * @return
+     */
+    @NonNull
+    @Override
+    public Result doWork() {
+        
+        return null;
+    }
+}
